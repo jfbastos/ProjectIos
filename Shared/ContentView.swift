857eaa66ce18e926f141937f9432876369922c76
João Felipe Bastos do Nascimento <57013713+jfbastos@users.noreply.github.com>
2023-10-02 06:35:03 -0400

    Initial commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by user216021 on 10/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/ProjectIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectIosApp.swift
+//  Shared
+//
+//  Created by user216021 on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

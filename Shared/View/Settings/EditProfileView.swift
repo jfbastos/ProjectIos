db8d74095fe701aeac2357a5aed1d875842bba4b
João Felipe Bastos do Nascimento <57013713+jfbastos@users.noreply.github.com>
2023-10-06 05:39:37 -0400

    EditProfile
    Start implementation of Edit Profile View

--- Shared/View/Settings/EditProfileView.swift
+++ Shared/View/Settings/EditProfileView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct EditProfileView: View {
+    @State private var fullname = ""
     var body: some View {
-        Text("Edit profile")
+        ZStack{
+            Color(.systemGroupedBackground).ignoresSafeArea()
+            VStack {
+                VStack{
+                    HStack {
+                        VStack{
+                            Image("Android_robot.svg")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 64, height: 64)
+                                .clipShape(Circle())
+                            
+                            Button(action: {}, label: {
+                                Text("Edit")
+                            })
+                        }
+                        Text("Enter your name or change your profile photo")
+                            .font(.system(size: 16))
+                            .foregroundColor(.gray)
+                            .padding([.bottom, .horizontal])
+                    }
+                    
+                    Divider()
+                        .padding(.horizontal)
+                    
+                    TextField("", text: $fullname)
+                        .padding(8)
+                }
+                .background(.white)
+                
+                VStack{
+                    HStack{
+                        Text("At the movies")
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                }
+                Spacer()
+            }
+        }
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationTitle("Edit Profile")
     }
 }
 
